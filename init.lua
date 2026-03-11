c35e8e5ac61f21504cb6b7a808a8b34252a54a47
Klemen Zaletelj <klemen.zaletelj@fs.uni-lj.si>
2026-03-11 08:52:33 +0100

    updates

--- init.lua
+++ init.lua
@@ -6,5 +6,6 @@ pref_by_location:setup({
   no_notify = false,
   prefs = {
     { location = ".*/Downloads", sort = { "mtime", reverse = true, dir_first = true }, linemode = "mtime" },
+    { location = ".*/RemminaShareFolder", sort = { "mtime", reverse = true, dir_first = true }, linemode = "mtime" },
   }
 })
